b4bb6a99ef368915e2e6907822d0d603307dd5c5
jon-rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-02-25 14:59:13 -0600

    initial commit
    Codewars JS 8kyu calculate the average solutions

---
+++ calculate_avg.js
@@ -0,0 +1,13 @@
+/*
+Write a function which calculates the average of the numbers in a given list.
+*/
+
+function find_average(array) {
+  // your code here
+  let sum =0
+  for(let i =0; i<array.length; i++){
+    sum += array[i]
+
+  }
+ return sum/(array.length)
+}
