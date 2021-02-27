4dc9a4f5b37ff467de6bef53bf84bf3f1ef469f5
jon-rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-02-27 14:31:44 -0600

    initial commit
    Codewayrs JS 8kyu  Double Char solution

---
+++ double_char.js
@@ -0,0 +1,19 @@
+/* Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  "
+Good Luck!
+*/
+
+function doubleChar(str) {
+  // Your code here
+  let arr = []
+  for(let i=0; i<str.length; i++){
+    arr.push(str[i])
+    arr.push(str[i])
+  }
+  return arr.join('')
+}
