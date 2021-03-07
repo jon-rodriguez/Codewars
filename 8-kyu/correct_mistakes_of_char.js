4e72b84516f3874c080cab9d7ccfa772530f8317
jon-rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-07 11:18:43 -0600

    move to 8-kyu folder from Codewars folder

--- 8-kyu/correct_mistakes_of_char.js
+++ 8-kyu/correct_mistakes_of_char.js
@@ -1,4 +1,5 @@
-/* Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+/* 
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
 
 When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
 
