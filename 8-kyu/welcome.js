545a7dac540155b316775037359fd8e204281794
jon-rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-07 14:18:45 -0600

    move to 8-kyu folder from Codewars folder

--- 8-kyu/welcome.js
+++ 8-kyu/welcome.js
@@ -1,4 +1,5 @@
-/* our start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+/* 
+our start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
 
 The Task
 Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
