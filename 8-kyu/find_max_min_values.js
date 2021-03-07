e8b695a9f9de6b7925d8a7f22a8532753ce91b9c
jon-rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-07 14:02:13 -0600

    move to 8-kyu folder from Codewars folder

--- 8-kyu/find_max_min_values.js
+++ 8-kyu/find_max_min_values.js
@@ -1,4 +1,5 @@
-/* Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+/* 
+Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
 
 #Examples
 
