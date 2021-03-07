278e0c22eb8eccb1764d083d39bc0cbf7cdac488
jon-rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-07 14:03:40 -0600

    move to 8-kyu folder from Codewars folder

--- 8-kyu/grasshopper_gradebook.js
+++ 8-kyu/grasshopper_gradebook.js
@@ -1,4 +1,5 @@
-/* Grade book
+/* 
+Grade book
 Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
 
 Numerical Score	Letter Grade
