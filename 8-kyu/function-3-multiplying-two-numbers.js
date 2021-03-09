0a9c56ee56a7ade4c2122a7f5974dbc931746726
jon-rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-09 15:02:45 -0600

    initial commit
    Codewars JavaScript Function 3 - multiplying two numbers solution

---
+++ 8-kyu/function-3-multiplying-two-numbers.js
@@ -0,0 +1,10 @@
+/*
+Function 3 - multiplying two numbers
+
+Implement a function which multiplies two numbers.
+*/
+
+// Write here your multiply-function
+const multiply = function(a,b){
+  return a*b
+}
