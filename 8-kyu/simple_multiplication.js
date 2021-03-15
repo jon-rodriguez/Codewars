0881ac7377877ecc29d6050c6cb6c5f01186730b
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 13:56:10 -0500

    Simple multiplication solution

--- 8-kyu/simple_multiplication.js
+++ 8-kyu/simple_multiplication.js
@@ -1,4 +1,8 @@
-/* This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+/* 
+Simple multiplication
+
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 */
 
 function simpleMultiplication(number) {
