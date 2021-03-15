6fcf7f6b9689269f781c404b4fba5eea599f7ce8
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 14:06:47 -0500

    Transportation on vacation solution

--- 8-kyu/transportation_on_vacation.js
+++ 8-kyu/transportation_on_vacation.js
@@ -1,4 +1,7 @@
 /* 
+Transportation on vacation
+
+
 After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
 
 You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
