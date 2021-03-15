6167f9efacccfe4d3428ba16b85deb1aa763cec2
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 13:22:48 -0500

    How good are you really? solution

--- 8-kyu/how_good_are_you_really.js
+++ 8-kyu/how_good_are_you_really.js
@@ -1,4 +1,6 @@
 /* 
+How good are you really? solution
+
 
 There was a test in your class and you passed it. Congratulations!
 But you're an ambitious person. You want to know if you're better than the average student in your class.
