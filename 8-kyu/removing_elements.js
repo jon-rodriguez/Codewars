1c45b5b38d88963a79e47f3724e02feabd3776cb
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 13:33:13 -0500

    Removing Elements solution

--- 8-kyu/removing_elements.js
+++ 8-kyu/removing_elements.js
@@ -1,4 +1,7 @@
 /*
+Removing Elements
+
+
 Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 
 Example:
@@ -7,7 +10,6 @@ myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
 */
 
 function removeEveryOther(arr){
-  //your code here
   let newArr = []
   for(let i = 0; i<arr.length; i+=2){
     newArr.push(arr[i])
