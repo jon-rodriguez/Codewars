23168d6c15a6863676e36f4d928eb696a3bb65f8
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 13:13:42 -0500

    Find Maximum and Minimum Values of a List solution

--- 8-kyu/find_max_min_values.js
+++ 8-kyu/find_max_min_values.js
@@ -1,4 +1,7 @@
 /* 
+Find Maximum and Minimum Values of a List solution
+
+
 Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
 
 #Examples
