8bf40f044fc725f83abb583d6d3458e352d452f5
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 14:02:19 -0500

    Sum without highest and lowest number solution

--- 8-kyu/sumwithouthighestandlowest.js
+++ 8-kyu/sumwithouthighestandlowest.js
@@ -1,4 +1,7 @@
 /*
+Sum without highest and lowest number
+
+
 Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
 (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
 
