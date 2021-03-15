d32eac6a0600a810febb252061799b1bb1bc080d
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 14:04:54 -0500

    Thinkful - Logic Drills: Traffic light solution

--- 8-kyu/thnkful_logicDrills_trafficLights.js
+++ 8-kyu/thnkful_logicDrills_trafficLights.js
@@ -1,4 +1,7 @@
 /*
+Thinkful - Logic Drills: Traffic light 
+
+
 You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
 Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
@@ -7,8 +10,6 @@ For example, update_light('green') should return 'yellow'.
 */
 
 function updateLight(current) {
-
-  //your code here!
   let colors = ["green","yellow","red"];
   if( colors.indexOf(current) == 2){
     return colors[0];
