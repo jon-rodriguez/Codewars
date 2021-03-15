c25186e9c34817b85a803054d52d8dd06c61c054
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 13:24:26 -0500

    Invert values solution

--- 8-kyu/invert_values.js
+++ 8-kyu/invert_values.js
@@ -1,4 +1,7 @@
 /*
+Invert values solution
+
+
 Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
 invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
