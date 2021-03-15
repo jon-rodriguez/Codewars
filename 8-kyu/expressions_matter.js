a855084c6d4d132616845c71f92bcda212f50ec7
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-15 13:09:29 -0500

    Expressions Matter solution

--- 8-kyu/expressions_matter.js
+++ 8-kyu/expressions_matter.js
@@ -1,4 +1,7 @@
 /*  
+Expressions Matter solution
+
+
 Task
 Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
 In other words , *_try every combination of a,b,c with [+()] , and return the Maximum Obtained_**
