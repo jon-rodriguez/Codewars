d27234f6c7c22093ff86c5abeb40719b7b27d5bd
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-18 17:46:16 -0500

    Highest and Lowest solution

--- 7-kyu/7kyu_highest_lowest.js
+++ 7-kyu/7kyu_highest_lowest.js
@@ -1,4 +1,8 @@
-/* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+/*
+Highest and Lowest
+
+
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 Example:
 
