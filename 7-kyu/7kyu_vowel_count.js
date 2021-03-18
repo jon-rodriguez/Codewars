769f582c6c7bd3f0248b67030f42307c0efb81cd
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-18 17:41:41 -0500

    Vowel Count solution

--- 7-kyu/7kyu_vowel_count.js
+++ 7-kyu/7kyu_vowel_count.js
@@ -1,4 +1,8 @@
-/* Return the number (count) of vowels in the given string.
+/* 
+Vowel Count
+
+
+Return the number (count) of vowels in the given string.
 
 We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
