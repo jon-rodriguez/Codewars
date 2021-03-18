bfaf81e7f0c3a723d6d47805c74c23e0b5ac9179
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-18 17:39:48 -0500

    You're a square! solution

--- 7-kyu/7kyu_youre_square.js
+++ 7-kyu/7kyu_youre_square.js
@@ -1,4 +1,7 @@
 /*
+You're a square!
+
+
 A square of squares
 You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 
