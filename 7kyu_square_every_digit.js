07065e614884ea57bf1a0e17ff8bf36fe7b9b146
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-03-18 17:44:04 -0500

    Square Every Digit solution

--- 7kyu_square_every_digit.js
+++ 7kyu_square_every_digit.js
@@ -1,4 +1,8 @@
-/* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+/* 
+Square Every Digit
+
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
