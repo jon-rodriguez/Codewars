21c78f465ef2e30d56f8ddfd6c707b2f03b94f7c
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-04-03 14:22:37 -0500

    initial commit

---
+++ remove-duplicates-from-list.js
@@ -0,0 +1,12 @@
+/*
+8kyu - Remove duplicates from list
+
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+*/
+
+function distinct(a) {
+  let arr = [...new Set(a)]
+  return arr
+}
