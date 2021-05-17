21039404df0f816ed6dda940f4b24c529db9d855
Jonathan Rodriguez <76972867+jon-rodriguez@users.noreply.github.com>
2021-05-16 23:36:46 -0500

    initial commit

---
+++ give-me-diamond.js
@@ -0,0 +1,41 @@
+/*
+6kyu - Give me a Diamond
+
+
+Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+Task
+You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+Examples
+A size 3 diamond:
+
+ *
+***
+ *
+...which would appear as a string of " *\n***\n *\n"
+
+A size 5 diamond:
+
+  *
+ ***
+*****
+ ***
+  *
+...that is:
+
+"  *\n ***\n*****\n ***\n  *\n"
+*/
+
+function diamond(n){
+  if (n < 1 || n % 2 == 0) return null;
+  return Array.apply(null, {length: n})
+    .map((e, i) => {
+      const spaces = Math.abs(i - (Math.floor(n / 2)));
+      const draw = n - spaces * 2;
+      return Array(spaces + 1).join(' ') + Array(draw + 1).join('*');
+    })
+    .join('\n') + '\n';
+  }
